ec770e03e534e3358cba8f4c11dd21209d1220e9
Raffi Arief Akbar <rapiariep@gmail.com>
2023-11-30 23:08:50 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+$config["server"]='localhost';
+$config["username"]='rapiarie_sbp';
+$config["password"]='makanbang123';
+$config["database_name"]='rapiarie_sbp';
---
+++ home.php
@@ -0,0 +1,10 @@
+<div class="page-header">
+<img src="./image/home1.jpg" width="400px" style="setImageOpacity(3.3);">
+<br>
+<br>
+    <h1 class="entry-title">Diagnosa Penyakit Lambung</h1>
+</div>
+<p class="entry-summary hidden">Sistem pakar diagnosa penyakit lambung pada manusia merupakan sistem yang digunakan untuk mendiagnosa jenis penyakit lambung pada manusia</p>
+<p class="entry-content" align="justify">Sistem pakar diagnosa penyakit lambung pada manusia merupakan sistem yang digunakan 
+untuk mendiagnosa jenis penyakit lambung pada manusia. 
+Sistem ini dimaksudkan untuk membantu dokter dalam menentukan penyakit serta membantu masyarakat umum untuk mengetahui jenis penyakit yang di derita secara dini.</p>
